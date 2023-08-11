998a5ea851729b8d9129d0358200cd239b69f4db
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-08-11 22:09:54 +0100

    js

--- Games/Tick0Tack Toe/js/app.js
+++ Games/Tick0Tack Toe/js/app.js
@@ -1,5 +1,11 @@
 const gameBoard = document.querySelector('#gameboard')
 const infoDisplay = document.querySelector('#info')
 const startCells = [
-    "",
+    "", "", "", "", "", "", "", "", ""
 ]
+
+function createBoard() {
+    startCells.forEach((cell, index) => {
+        const cellElement = document.createElement('div')
+    })
+}
