14d1036f9739c6ed200bb914416edbb9fb3a89ee
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-08-14 16:01:12 +0100

    JS

--- Games/Tick0Tack Toe/New Research/For Loop/forLoop.js
+++ Games/Tick0Tack Toe/New Research/For Loop/forLoop.js
@@ -1,7 +1,15 @@
-for (let step = 4; step < 6; step++) {
+for (let step = 0; step < 6; step++) {
   console.log("Juicy femboy ass");
 }
 
-for (let step = 0; step < 6; step++) {
+for (let step = 4; step < 6; step++) {
   console.log("Tiny tight femboy");
+}
+
+for (let step = 4; step > 6; step++) {
+  console.log("femboy slut beatifull hole");
+}
+
+for (let step = 100; step < 155; step++) {
+  console.log("kissable cute femboy ass");
 }
