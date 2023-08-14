17a5b2eb0c3fcdc7ec2c421a0a039dea5bc8fcaa
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-08-14 16:16:59 +0100

    Make Shift Changes

---
+++ Games/Tick0Tack Toe/New Research/For Loop/02 ForLoop/forLoop.js
@@ -0,0 +1,18 @@
+for (let step = 0; step < 6; step++) {
+  console.log("01: Juicy femboy ass");
+}
+
+for (let step = 4; step < 6; step++) {
+  console.log("02: Tiny tight femboy");
+}
+for (let step = 100; step < 155; step++) {
+  console.log("04: kissable cute femboy ass");
+}
+
+for (let step = 5; step > 6; step++) {
+  console.log("03: femboy slut beatifull hole");
+}
+
+/** If you make the number of steps bigger than the second condition
+ * then you get a infinite loop.
+ */
