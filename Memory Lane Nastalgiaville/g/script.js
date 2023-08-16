0de0323c051277e076e82f0e651675d56720a7ef
archfiend-Tanvir <tanvirelhussain@gmail.com>
2023-08-16 21:21:27 +0100

    JS

---
+++ Memory Lane Nastalgiaville/g/script.js
@@ -0,0 +1,43 @@
+/**
+document.querySelector(".").addEventListener("click", function () {
+  window.open(
+    "",
+    "_blank",
+    "width=500, height=600, top=100, left=400"
+  );
+});
+
+ * 
+ */
+
+document.querySelector(".btn").addEventListener("click", function () {
+  window.open(
+    "https://www.gayguides.com/forums/uploads/monthly_02_2017/post-8337-0-29486400-1486159157.jpg",
+    "_blank",
+    "width=500, height=600, top=100, left=400"
+  );
+});
+
+document.querySelector(".btn02").addEventListener("click", function () {
+  window.open(
+    "https://www.gayguides.com/forums/uploads/monthly_02_2017/post-10915-0-61959700-1486166832.jpg",
+    "_blank",
+    "width=800, height=600, top=100, left=400"
+  );
+});
+
+document.querySelector(".btn03").addEventListener("click", function () {
+  window.open(
+    "https://www.gayguides.com/forums/uploads/monthly_02_2017/post-8337-0-68975700-1486408602.jpg",
+    "_blank",
+    "width=500, height=600, top=100, left=400"
+  );
+});
+
+document.querySelector(".btn04").addEventListener("click", function () {
+  window.open(
+    "https://www.gayguides.com/forums/uploads/monthly_02_2017/post-8337-0-61635800-1486408612.jpg",
+    "_blank",
+    "width=500, height=600, top=100, left=400"
+  );
+});
